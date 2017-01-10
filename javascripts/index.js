7adc887f7d343b4eade3d1758504cdcb61650da0
Daniel Gorelick <dqgorelick@gmail.com>
2017-01-10 18:51:12 -0500

    adds crossing detection

--- javascripts/index.js
+++ javascripts/index.js
@@ -34,28 +34,46 @@
     renderer.render(stage);
   }
 
-
   var line = [];
-  var lastPoint;
   var marker = new PIXI.Graphics();
   stage.addChild(marker);
-  var oldPoint, oldMidPoint;
+  var lastPoint;
+  var lastMidPoint;
+
+  function addNode(point) {
+    marker.drawCircle(point.x, point.y, 10);
+  }
+
+  function checkLine(touch, lastTouch) {
+    var lineCenter = window.innerHeight/2;
+    if ((touch.y >= lineCenter && lastTouch.y < lineCenter) | (touch.y <= lineCenter && lastTouch.y > lineCenter)) {
+      var m = (lastTouch.y - touch.y) / (lastTouch.x - touch.x);
+      var dy =  lastTouch.y - lineCenter;
+      var x_tar = (m*lastTouch.x - dy) / m;
+
+      var direction = touch.y > lastTouch.y ? 'down' : 'up';
+      console.log(direction);
+      if (!!x_tar) {
+        addNode({x: x_tar, y:lineCenter});
+      } else {
+        addNode({x: lastTouch.x, y:lineCenter});
+      }
+    }
+  }
 
   function onDragStart(event) {
     this.data = event.data;
     this.dragging = true;
-
-      var point = new PIXI.Point(event.data.global.x, event.data.global.y);
-      var x = event.data.global.x;
-      var y = event.data.global.y;
-
-      marker.clear().beginFill(0xffd900);
-      marker.lineStyle(10, 0xffd900);
-      marker.moveTo(point.x, point.y);
-      marker.lineTo(point.x, point.y);
-      marker.drawCircle(point.x, point.y, 0.4);
-      oldPoint = oldMidPoint = point;
-      renderer.render(stage);
+    var point = new PIXI.Point(event.data.global.x, event.data.global.y);
+    line = [];
+    marker.clear().beginFill(0xffd900);
+    marker.lineStyle(10, 0xffd900);
+    marker.moveTo(point.x, point.y);
+    marker.lineTo(point.x, point.y);
+    marker.drawCircle(point.x, point.y, 0.4);
+    lastPoint = lastMidPoint = point;
+    renderer.render(stage);
+    line.push({x: point.x, y: point.y});
   }
 
   function onDragEnd(event) {
@@ -68,12 +86,16 @@
   function onDragMove(event) {
     if(this.dragging) {
       var point = new PIXI.Point(event.data.global.x, event.data.global.y);
-      var midPoint = new PIXI.Point(oldPoint.x + point.x >> 1, oldPoint.y + point.y >> 1);
+      var midPoint = new PIXI.Point(lastPoint.x + point.x >> 1, lastPoint.y + point.y >> 1);
+      if (lastPoint) {
+        checkLine(point, lastPoint);
+      }
       marker.moveTo(midPoint.x, midPoint.y);
-      marker.quadraticCurveTo(oldPoint.x, oldPoint.y, oldMidPoint.x, oldMidPoint.y);
+      marker.quadraticCurveTo(lastPoint.x, lastPoint.y, lastMidPoint.x, lastMidPoint.y);
       marker.drawCircle(midPoint.x, midPoint.y, 0.4);
-      oldMidPoint = midPoint;
-      oldPoint = point;
+      lastMidPoint = midPoint;
+      lastPoint = point;
+      line.push({x: point.x, y: point.y});
       renderer.render(stage);
     }
   }
